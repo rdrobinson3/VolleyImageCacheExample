527e98290d0b5d59eae2313072736eb6cb64a7b1
Steven Byle <stevo324@gmail.com>
2013-05-29 15:15:29 -0400

    Improvements to pagination speed, pagination logic, and uri creation. Also removed menu items as they are not used. Updated minimum sdk to API 7.

--- CaptechBuzz/src/com/captechconsulting/captechbuzz/MainActivity.java
+++ CaptechBuzz/src/com/captechconsulting/captechbuzz/MainActivity.java
@@ -1,5 +1,15 @@
 package com.captechconsulting.captechbuzz;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
 import com.android.volley.Response;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
@@ -7,44 +17,28 @@ import com.captechconsulting.captechbuzz.BuzzArrayAdapter.ViewHolder;
 import com.captechconsulting.captechbuzz.model.twitter.TweetData;
 import com.captechconsulting.captechbuzz.model.twitter.TwitterManager;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
-
 public class MainActivity extends Activity {
 
-	private final String TAG = getClass().getSimpleName(); 
+	private final String TAG = getClass().getSimpleName();
 	private ListView mListView;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mListView = (ListView)findViewById(R.id.buzzListView);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	@Override
 	protected void onResume() {
 		super.onResume();
-		
-		if(mListView.getAdapter() == null)
-			TwitterManager.getInstance().getDefaultHashtagTweets(createMyReqSuccessListener(), createMyReqErrorListener(), null);
-		
+
+		if(mListView.getAdapter() == null) {
+			// Get the first page
+			TwitterManager.getInstance().getDefaultHashtagTweets(createMyReqSuccessListener(), createMyReqErrorListener(), null, 1);
+		}
+
 		mListView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
@@ -56,29 +50,29 @@ public class MainActivity extends Activity {
 					Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(holder.destinationUrl));
 					startActivity(intent);
 				}
-				
+
 			}
 		});
-		
+
 	}
-	
-	 private Listener<TweetData> createMyReqSuccessListener() {
-	        return new Listener<TweetData>() {
-	            @Override
-	            public void onResponse(TweetData response) {
-	            	Log.v(TAG, "Tweet data loaded");
-	            	mListView.setAdapter(new BuzzArrayAdapter(MainActivity.this, response));
-	            }
-	        };
-	 }
-	 
-	 private Response.ErrorListener createMyReqErrorListener() {
-	        return new Response.ErrorListener() {
-	            @Override
-	            public void onErrorResponse(VolleyError error) {
-	            	Log.e(TAG, "Tweet data failed to load");
-	            }
-	        };
-	    }
+
+	private Listener<TweetData> createMyReqSuccessListener() {
+		return new Listener<TweetData>() {
+			@Override
+			public void onResponse(TweetData response) {
+				Log.v(TAG, "Tweet data loaded");
+				mListView.setAdapter(new BuzzArrayAdapter(MainActivity.this, response));
+			}
+		};
+	}
+
+	private Response.ErrorListener createMyReqErrorListener() {
+		return new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				Log.e(TAG, "Tweet data failed to load");
+			}
+		};
+	}
 
 }
--- CaptechBuzz/src/com/captechconsulting/captechbuzz/model/twitter/Tweet.java
+++ CaptechBuzz/src/com/captechconsulting/captechbuzz/model/twitter/Tweet.java
@@ -10,25 +10,29 @@ import com.google.gson.annotations.SerializedName;
 public class Tweet {
 
 	private final static String TWEET_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss Z";
-	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat(TWEET_DATE_FORMAT, Locale.US);
+	static {
+		sdf.setLenient(true);
+	}
+
 	@SerializedName("from_user")
-	private String username;
-	
-	
+	private final String username;
+
+
 	@SerializedName("text")
-	private String message;
-	
+	private final String message;
+
 	@SerializedName("profile_image_url")
-	private String userImageUrl;
-	
+	private final String userImageUrl;
+
 	@SerializedName("created_at")
 	private String createdAt;
-	
+
 	@SerializedName("id")
 	private String tweetId;
-	
+
 	private Date createdDate;
-	
+
 	public Tweet(String username, String message, String userImageUrl){
 		this.username = username;
 		this.message = message;
@@ -42,15 +46,15 @@ public class Tweet {
 	public String getMessage() {
 		return message;
 	}
-	
+
 	public String getUserImageUrl() {
 		return userImageUrl;
-	}	
-	
+	}
+
 	public String getCreatedAt() {
 		return createdAt;
 	}
-	
+
 	public String getDestinationUrl() {
 		return "https://twitter.com/" + username + "/status/" + tweetId;
 	}
@@ -58,15 +62,13 @@ public class Tweet {
 	public Date getCreatedDate(){
 		if(createdDate == null){
 			try {
-				SimpleDateFormat sdf = new SimpleDateFormat(TWEET_DATE_FORMAT, Locale.ENGLISH);
-				sdf.setLenient(true);
 				createdDate = sdf.parse(createdAt);
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return createdDate;
-		
+
 	}
 }
